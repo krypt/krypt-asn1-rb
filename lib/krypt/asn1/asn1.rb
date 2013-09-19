a33e0738b2b7ee7bad63c716727811c3798a3a13
emboss <Martin.Bosslet@googlemail.com>
2013-09-19 23:00:52 +0200

    Add Asn1.decode module function.

--- lib/krypt/asn1/asn1.rb
+++ lib/krypt/asn1/asn1.rb
@@ -1,3 +1,13 @@
 require_relative 'asn1/primitive'
 require_relative 'asn1/constructed'
 require_relative 'asn1/parser'
+
+module Krypt::ASN1
+
+  module_function
+
+  def decode(io_or_string)
+    Krypt::Asn1::Parser.new.parse(io_or_string)
+  end
+
+end
